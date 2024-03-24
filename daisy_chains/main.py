06c893970bf6143602123e34ed4e2e8a4eef5c91
Dheeraj Vislawath <57468596+DeerEdge@users.noreply.github.com>
2024-03-24 10:42:16 -0600

    daisy chains solved

--- daisy_chains/main.py
+++ daisy_chains/main.py
@@ -1,1 +1,17 @@
+import sys
+import math
 
+input = sys.stdin.readline
+
+N = int(input())
+flowers = list(int(size) for size in input().split())
+
+avg_flowers = N
+
+for i in range(N):
+    for j in range(i+1, N):
+        avg = sum(flowers[i:j+1])/len(flowers[i:j+1])
+        if (avg in flowers[i:j+1]):
+            avg_flowers += 1
+
+sys.stdout.write(str(avg_flowers))
